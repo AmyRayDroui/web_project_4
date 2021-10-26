f7bc5faa2befb377647dcee5c8f18e926b112360
AmyRayDroui <amyraydroui@gmail.com>
2021-10-27 01:14:47 +0200

    finished project

--- scripts/index.js
+++ scripts/index.js
@@ -31,6 +31,7 @@ let openAddButton = document.querySelector('.profile__button_type_add-image');
 
 let popupEditProfile = document.querySelector('.popup_type_edit-profile');
 let popupAddCard = document.querySelector('.popup_type_add-card');
+let popupViewCard = document.querySelector('.popup_type_card-view');
 let allCloseButtons = document.querySelectorAll('.popup__close-button');
 
 let submitEditForm = popupEditProfile.querySelector('.popup__form');
@@ -56,12 +57,21 @@ function createCard(card) {
   cardElement.querySelector('.image-card__name').textContent = card.name;
   cardElement.querySelector('.image-card__image').style.backgroundImage = `url(${card.link})`;
 
+  //like button event
   cardElement.querySelector('.image-card__love-button').addEventListener('click', function(evt) {
     evt.target.classList.toggle('image-card__love-button_active');
   });
 
+  //view card popup event
+  cardElement.querySelector('.image-card__image').addEventListener('click', () => {
+    popupViewCard.querySelector('.popup__image').src = card.link;
+    popupViewCard.querySelector('.popup__card-title').textContent = card.name;
+    popupViewCard.classList.toggle('popup_visible');
+  })
+
+  //delete card event
   cardElement.querySelector('.image-card__remove-button').addEventListener('click', function(evt) {
-    evt.target.parentElement.parentElement.remove();
+    evt.target.parentElement.remove();
   })
 
   return cardElement;
@@ -97,6 +107,7 @@ allCloseButtons.forEach(button => button.addEventListener('click', () => {
   allPopups.forEach(popup => popup.classList.remove('popup_visible'));
 }));
 
+//submitting forms events
 submitEditForm.addEventListener('submit', () => handleEditSubmit(event, popupEditProfile));
 submitAddForm.addEventListener('submit', () => handleAddSubmit(event, popupAddCard));
 
